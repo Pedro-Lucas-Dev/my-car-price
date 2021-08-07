26c3d51f4adade28002abc29a3feca8832847700
Pedro Lucas <Pedrolucas969210913@mgail.com>
2021-08-07 17:49:17 -0300

    feat: create layout component

---
+++ src/components/Layout/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Grid } from "@material-ui/core";
+
+const Layout = ({ children }) => {
+  return <Grid container>{children}</Grid>;
+};
+
+export default Layout;
