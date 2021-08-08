8d2a4e586c9ebd838b722494a514cf0640b3cf95
Pedro Lucas <Pedrolucas969210913@mgail.com>
2021-08-08 19:38:54 -0300

    feat: create autocomplete component

---
+++ src/components/Autocomplete/index.js
@@ -0,0 +1,25 @@
+import { Autocomplete as AutocompleteMaterial } from "@material-ui/lab";
+import { Grid, TextField } from "@material-ui/core";
+import React from "react";
+
+const Autocomplete = ({ id, value, options, label, onChange, disabled }) => {
+  return (
+    <Grid item xs>
+      <AutocompleteMaterial
+        id={id}
+        value={value}
+        options={options}
+        groupBy={(option) => option[0].toUpperCase()}
+        getOptionLabel={(option) => option}
+        style={{ margin: 10, padding: 10, width: 300 }}
+        onChange={onChange}
+        renderInput={(params) => (
+          <TextField {...params} label={label} variant="outlined" />
+        )}
+        disabled={disabled}
+      />
+    </Grid>
+  );
+};
+
+export { Autocomplete };
