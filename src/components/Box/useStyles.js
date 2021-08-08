b713b38f57bd8247b5c82ce3432516bb59415215
Pedro Lucas <Pedrolucas969210913@mgail.com>
2021-08-08 16:31:52 -0300

    feat: create style of Box component

--- src/components/Box/useStyles.js
+++ src/components/Box/useStyles.js
@@ -2,6 +2,7 @@ import { makeStyles } from "@material-ui/core";
 
 export const useStyles = makeStyles({
   paper: {
+    display: "flex",
     background: "#DEFFB9",
     borderRadius: 8,
     marginTop: 40,
