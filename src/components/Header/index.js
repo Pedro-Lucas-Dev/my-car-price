fad4aea4e4877f94dc4dab6ba32b2f76c6a9e51a
Pedro Lucas <Pedrolucas969210913@mgail.com>
2021-08-08 16:32:23 -0300

    feat: add icon component to Header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,17 +1,29 @@
 import React from "react";
 import { Grid } from "@material-ui/core";
-import Text from "../Text";
+import { Text } from "../Text";
+import { Icon } from "../Icon";
 
 const Header = () => {
   return (
-    <Grid container justifyContent="center" alignItems="center">
-      <Text
-        description="Quanto vale meu carro?"
-        variant="h4"
-        color="textPrimary"
-      />
+    <Grid
+      container
+      direction="row"
+      justifyContent="center"
+      alignItems="center"
+      spacing={2}
+    >
+      <Grid item>
+        <Icon />
+      </Grid>
+      <Grid item>
+        <Text
+          description="Quanto vale meu carro?"
+          variant="h4"
+          color="textPrimary"
+        />
+      </Grid>
     </Grid>
   );
 };
 
-export default Header;
+export { Header };
