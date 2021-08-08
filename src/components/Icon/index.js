3bfb517539cc5deece106aafa2326606b8a5df7f
Pedro Lucas <Pedrolucas969210913@mgail.com>
2021-08-08 16:30:53 -0300

    feat: create icon component

---
+++ src/components/Icon/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.css";
+
+const Icon = () => {
+  return (
+    <img
+      className="Img"
+      src="https://www.mobills.com.br/blog/wp-content/uploads/2021/03/Creditas.png"
+      alt="Logo"
+    />
+  );
+};
+
+export { Icon };
