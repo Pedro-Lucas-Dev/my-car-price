c6b18fa1ccdc769137ae435eb5a5943de292be2a
Pedro Lucas <Pedrolucas969210913@mgail.com>
2021-09-29 21:58:28 -0300

    feat: add form to Main

--- src/modules/Main/index.js
+++ src/modules/Main/index.js
@@ -1,11 +1,13 @@
 import React from "react";
-import Layout from "../../components/Layout";
-import Header from "../../components/Header";
+import { Layout } from "../../components/Layout";
+import { Header } from "../../components/Header";
+import { Form } from "../../components/Form";
 
 const Main = () => {
   return (
     <Layout>
       <Header />
+      <Form />
     </Layout>
   );
 };
